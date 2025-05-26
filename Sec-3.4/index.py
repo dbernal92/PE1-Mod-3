7d58c69427c614c319e64adde19682b62e4511eb
Darien Bernal <darien.bernal@gmail.com>
2025-05-25 23:57:06 -0400

    Inequality operator

--- Sec-3.4/index.py
+++ Sec-3.4/index.py
@@ -1,7 +1,15 @@
 # 3.1.4 Operators
 
+# Equality: the equal to operator (==)
 var = 0  # Assigning 0 to var
 print(var == 0)
 
 var = 1  # Assigning 1 to var
 print(var == 0)
+
+# Inequality: the not equal to operator (!=)
+var = 0  # Assigning 0 to var
+print(var != 0)
+ 
+var = 1  # Assigning 1 to var
+print(var != 0)
