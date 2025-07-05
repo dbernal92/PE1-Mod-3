d9dbef132a4dcbbad443212c90e9d513b34078c4
Darien Bernal <darien.bernal@gmail.com>
2025-07-05 12:46:55 -0400

    Correct tax if income is negative

--- Lab-3.1.11/index.py
+++ Lab-3.1.11/index.py
@@ -16,7 +16,15 @@ income = float(input("Enter the annual income: "))
 
 if income < 85528:
 	tax = income * 0.18 - 556.02
-# Write the rest of your code here.
+
+if income > 85528:
+    tax = ((85528 - income) * 0.32) + 14839.02
+
+if income == 0:
+    tax = 0
+    
+if tax < 0: # If the calculated tax is less than zero, set it to zero
+    tax = 0
 
 tax = round(tax, 0)
 print("The tax is:", tax, "thalers")
